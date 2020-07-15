a967eeaf67e24f4ebc81bfbfdd3877bc8bd7da76
DegrangeM <53106394+DegrangeM@users.noreply.github.com>
2020-07-15 14:04:32 +0200

    v0.1.0

---
+++ index.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+const crypto = require('crypto');
+
+app.use(express.static('public'));
+
+const SALT = '2j3dWJRwFkG3v3U9';
+const TEACHER_PASSWORD = 'N7ykH8SMA8pvz4F5';
+
+/*
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+app.get('/teacher', (req, res) => {
+  res.sendFile(__dirname + '/teacher.html');
+});
+app.get('/student', (req, res) => {
+  res.sendFile(__dirname + '/student.html');
+});
+*/
+
+http.listen(3000, () => {
+  console.log('go to http://localhost:3000');
+});
+
+
+io.on('connect', socket => {
+	socket.on('teacher', (pw, fn) => {
+		if(pw === undefined || fn === undefined) return;
+		if(pw.substr(1) == TEACHER_PASSWORD) {
+			socket.join('teacher');
+			socket.to('student').emit('who');
+			socket.on('create-link', (data, fn) => {
+				fn(crypto.createHmac('sha256', SALT).update(data).digest('hex').substr(0,5));
+			});
+			fn(true);
+		} else {
+			fn(false);
+		}
+	});
+	socket.on('student', (data, fn) => {
+		if(data === undefined || fn === undefined) return;
+		let datas = data.substr(1).split('/');
+		let name = decodeURIComponent(datas[0]);
+		let hash = datas[1];
+		let goodhash = crypto.createHmac('sha256', SALT).update(name).digest('hex').substr(0,5);
+
+		if(hash == goodhash) {
+			socket.name = name;
+			socket.join('student');
+			socket.to('teacher').emit('student-connect', socket.id, socket.name);
+			socket.on('screenshot', data => {
+				socket.to('teacher').emit('screenshot', socket.id, data);
+			});
+			socket.on('disconnect', () => {
+				socket.to('teacher').emit('student-disconnect', socket.id);;
+			});
+			socket.on('here', () => {
+				socket.to('teacher').emit('here', socket.id, socket.name);
+			});
+			fn(true);
+		} else {
+			fn(false);
+		}
+	});
+	
+});
